e09846ed96f4b766e341d5c433c9a9abf3a1321e
siport <siport@microsoft.com>
2016-02-25 17:21:17 -0800

    fix madagascar

--- ElasticSearchMSBand/Constants.cs
+++ ElasticSearchMSBand/Constants.cs
@@ -8,6 +8,6 @@
         public const string ConnectionString = "HostName=HckthnESBandS1.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GNJMLRz7/h2MpRBgu+VtDiZUjsgo+3YGfXKjoCvjcKo=";
 
         public static int NumDevices = 10000;
-        public static TimeSpan SimulationDelay = TimeSpan.FromSeconds(60);
+        public static TimeSpan SimulationDelay = TimeSpan.FromSeconds(180);
     }
 }
