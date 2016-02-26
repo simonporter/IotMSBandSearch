62a0262282a3296784f4f468d85049472fc51295
siport <siport@microsoft.com>
2016-02-26 11:08:37 -0800

    add miami

--- ElasticSearchMSBand/Constants.cs
+++ ElasticSearchMSBand/Constants.cs
@@ -7,8 +7,8 @@
         // free hub public const string ConnectionString = "HostName=HckthnESBand.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=pKMn+3nw38PdcfoTHMIeY/HwutiuSJKsJ6MciG+i4g4=";
         public const string ConnectionString = "HostName=HckthnESBandS1.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GNJMLRz7/h2MpRBgu+VtDiZUjsgo+3YGfXKjoCvjcKo=";
 
-        public static int DeviceSeed = 2000;
+        public static int DeviceSeed = 0;
         public static int NumDevices = 1000;
-        public static TimeSpan SimulationDelay = TimeSpan.FromSeconds(60);
+        public static TimeSpan SimulationDelay = TimeSpan.FromSeconds(300);
     }
 }
